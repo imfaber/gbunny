03975dbca144c8d0314cc6dd01287b0925fa0f26
Fabrizio Meinero <fabrizio@imfaber.me>
2020-04-28 09:28:13 +0200

    Add script for dev

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-// @flow
-
 /**
  * This function says hello.
  * @param name Some name to say hello for.
  * @returns The hello.
  */
-const sayHello = (name: string = "Haz"): string => `Hello, ${name}!`;
+const sayHello = (name: string = "Fab"): string => `Hello, ${name}!`;
+
+console.log(sayHello());
 
 export default sayHello;
