7bdfa349b8f3f274b7df5580b57c2511f202370f
Fabrizio Meinero <fabrizio@imfaber.me>
2020-05-01 21:23:41 +0200

    Print current branch

--- src/status.ts
+++ src/status.ts
@@ -1,14 +1,25 @@
 #!/usr/bin/env node
 
-import git from 'simple-git/promise';
+import git from './git';
 import clear from 'clear';
+import chalk from 'chalk';
+
+const log = console.log;
 
 const showStatus = async () => {
-    const s = await git().status();
+    const s = await git.status();
 
     clear();
 
-    console.log(s);
+    const tracking = s.tracking
+        ? ` | [${chalk.white(s.tracking)}]`
+        : null;
+
+    log(
+        chalk.grey(
+            `On branch: ${chalk.white(s.current)}${tracking ?? ''}`
+        )
+    );
 
     return s;
 };
