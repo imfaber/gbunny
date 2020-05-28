463872df91698c6a0e8c6ecaed868c27af84d053
Fabrizio Meinero <fabrizio@imfaber.me>
2020-05-28 11:08:45 +0200

    Remove unecessary test

--- test/unit/common/has-color-option.spec.ts
+++
@@ -1,8 +0,0 @@
-import hasColorOption from '@/common/has-color-option';
-
-describe('hasColorrOption()', () => {
-    it('should determine wheter the command has the color option', async () => {
-        expect(await hasColorOption('log')).toBeTruthy();
-        expect(await hasColorOption('commit')).toBeFalsy();
-    });
-});
